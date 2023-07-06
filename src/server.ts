2ff371137f1a641f43f777ba45c7e3f8a8ca3ba4
gersom911 <familiacedram@gmail.com>
2023-07-06 12:05:04 -0500

    politicas seguridad y morgan

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
-import express from 'express';
-
+import express from "express";
+import helmet from "helmet";//politicas de seguridad
+import morgan from 'morgan';//monitorear empoind
 const app = express();
+/********middleware generales********/
+app.use(express.json());
+app.use(express.urlencoded({ extended:false}))
 
+app.use(helmet());//configuro politicas
+app.use(morgan('dev'));//configuro morgan
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`LA APP A ARRANCADO EN LA http://localhost:${PORT}`);
+})
 
-app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
-});
+
